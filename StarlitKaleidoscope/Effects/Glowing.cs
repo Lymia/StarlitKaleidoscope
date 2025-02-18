6e58ed3f793a2c8fe79f08f057b9e972e0b4b58e
Rimi Kanokawa <rimi@rimin.moe>
2025-02-17 22:45:40 -0800

    Initial version of the Light Manipulation rework MVP.

---
+++ StarlitKaleidoscope/Effects/Glowing.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using XRL.Rules;
+using XRL.World;
+
+namespace Mods.StarlitKaleidoscope.Effects {
+    [Serializable]
+    public class Glowing : Effect, ITierInitialized {
+        public int GlowRadius;
+        public int DvPenalty;
+
+        public Glowing() {
+            DisplayName = "{{Y|glowing}}";
+            GlowRadius = 2;
+            DvPenalty = 1;
+            Duration = 10;
+        }
+
+        public Glowing(int glowRadius, int dvPenalty, int duration) {
+            GlowRadius = glowRadius;
+            DvPenalty = dvPenalty;
+            Duration = duration;
+        }
+
+        public void Initialize(int Tier) {
+            GlowRadius = 2 + Tier / 3;
+            DvPenalty = 1 + Tier / 2;
+            Duration = Stat.Random(10, 30);
+        }
+        
+        public override bool UseStandardDurationCountdown() => true;
+        public override string GetDetails() => "-" + DvPenalty + " DV";
+        public override string GetDescription() => "{{Y|glowing ({{C|-" + DvPenalty + " DV}})}}";
+        public override string GetStateDescription() => "{{Y|glowing}}";
+        
+        public override bool WantEvent(int ID, int cascade) {
+            return base.WantEvent(ID, cascade) || ID == BeforeRenderEvent.ID;
+        }
+
+        public override bool HandleEvent(BeforeRenderEvent E) {
+            AddLight(GlowRadius);
+            return base.HandleEvent(E);
+        }
+
+        public override bool Apply(GameObject Object) {
+            ApplyStats();
+            return true;
+        }
+
+        public override void Remove(GameObject Object) => UnapplyStats();
+
+        void ApplyStats() => this.StatShifter.SetStatShift("DV", -this.DvPenalty);
+
+        void UnapplyStats() => this.StatShifter.RemoveStatShifts();
+    }
+}
---
+++ StarlitKaleidoscope/MutationReworks/LightManipulationPatch.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using HarmonyLib;
+using Mods.StarlitKaleidoscope.Effects;
+using XRL.World;
+using XRL.World.Parts.Mutation;
+
+// ReSharper disable UnusedType.Global
+// ReSharper disable UnusedMember.Local
+namespace Mods.StarlitKaleidoscope.MutationReworks {
+    public static class LightManipulationPatch {
+        public static int GetMaxLightRadius(int Level) =>
+            4 + Level / 2 + GetMinLightRadius(Level);
+
+        public static int GetMinLightRadius(int Level) =>
+            2 + (Level - 1) / 4;
+
+        public static int GetDvPenalty(int Level) =>
+            1 + (Level + 1) / 4;
+
+        public static int GetGlowDuration(int Level) =>
+            9 + Level;
+
+        public static int MaxUsedCharges(LightManipulation self) =>
+            GetMaxLightRadius(self.Level) - GetMinLightRadius(self.Level);
+
+        public static void ApplyGlowingEffect(LightManipulation self, GameObject target) {
+            target.ApplyEffect(new Glowing(
+                GetMinLightRadius(self.Level), GetDvPenalty(self.Level), GetGlowDuration(self.Level)
+            ));
+        }
+    }
+
+    // low level functions
+    public static class LightManipulationPatchLL {
+        [ThreadStatic]
+        static GameObject laseTarget;
+
+        public static GameObject WithLaseTarget(GameObject target) {
+            laseTarget = target;
+            return target;
+        }
+
+        public static void ApplyGlowingEffect(LightManipulation self) {
+            LightManipulationPatch.ApplyGlowingEffect(self, laseTarget);
+        }
+        
+        internal static void FinalizeCall() {
+            laseTarget = null;
+        }
+    }
+    
+    // Patch max radius
+    [HarmonyPatch(typeof(LightManipulation), nameof(LightManipulation.GetMaxLightRadius))]
+    internal static class LightManipulationPatchRadius {
+        static bool Prefix(out int __result, int Level) {
+            __result = LightManipulationPatch.GetMaxLightRadius(Level);
+            return false;
+        }
+    }
+    
+    // Patch minimum radius
+    [HarmonyPatch(typeof(LightManipulation), nameof(LightManipulation.HandleEvent), typeof(CommandEvent))]
+    internal static class LightManipulationPatchHandleEventCommandEvent {
+        internal static void PatchLightRadius(CodeMatcher codeMatcher) {
+            // replace the call to get_MaxLightRadius with a call to maxUsedCharges
+            codeMatcher
+                .MatchStartForward(
+                    new CodeMatch(
+                        OpCodes.Call,
+                        AccessTools.DeclaredPropertyGetter(typeof(LightManipulation), "MaxLightRadius")
+                    )
+                )
+                .ThrowIfInvalid("Could not find call to LightManipulation.get_MaxLightRadius")
+                .Repeat(
+                    matchAction: cm => {
+                        cm.RemoveInstruction();
+                        cm.InsertAndAdvance(
+                            CodeInstruction.Call(() => LightManipulationPatch.MaxUsedCharges(default))
+                        );
+                    }
+                );
+        }
+        
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions) {
+            var codeMatcher = new CodeMatcher(instructions);
+            PatchLightRadius(codeMatcher);
+            return codeMatcher.Instructions();
+        }
+    }
+    
+    // Patch max charges display
+    [HarmonyPatch(typeof(LightManipulation), nameof(LightManipulation.SyncAbilityName))]
+    internal static class LightManipulationPatchSyncAbilityName {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions) {
+            var codeMatcher = new CodeMatcher(instructions);
+            LightManipulationPatchHandleEventCommandEvent.PatchLightRadius(codeMatcher);
+            return codeMatcher.Instructions();
+        }
+    }
+    
+    // Patch lase glowing effect
+    [HarmonyPatch(typeof(LightManipulation), nameof(LightManipulation.Lase))]
+    internal static class LightManipulationPatchLase {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions) {
+            var codeMatcher = new CodeMatcher(instructions);
+            
+            // store the lase target
+            codeMatcher
+                .MatchStartForward(
+                    new CodeMatch(
+                        OpCodes.Callvirt, 
+                        AccessTools.Method(typeof(Cell), nameof(Cell.GetCombatTarget))
+                    )
+                )
+                .ThrowIfInvalid("Could not find call to Cell.GetCombatTarget")
+                .Advance(1)
+                .InsertAndAdvance(CodeInstruction.Call(() => LightManipulationPatchLL.WithLaseTarget(default)));
+            
+            // inject the glowing effect call
+            codeMatcher
+                .MatchStartForward(
+                    new CodeMatch(() => default(GameObject).TakeDamage(default, default, default))
+                )
+                .ThrowIfInvalid("Could not find call to GameObject.TakeDamage")
+                .Advance(1)
+                .InsertAndAdvance(new CodeInstruction(OpCodes.Ldarg_0)) // this
+                .InsertAndAdvance(
+                    CodeInstruction.Call(() => LightManipulationPatchLL.ApplyGlowingEffect(default))
+                );
+        
+            return codeMatcher.Instructions();
+        }
+
+        static void Finalizer() {
+            LightManipulationPatchLL.FinalizeCall();
+        }
+    }
+}
