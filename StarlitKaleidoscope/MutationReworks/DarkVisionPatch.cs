d292cafb1a08f75bf4acda690c9ab3fb440d8ab7
Rimi Kanokawa <rimi@rimin.moe>
2025-02-17 23:56:07 -0800

    Add the (pretty trivial) Night Vision mutation patch.

---
+++ StarlitKaleidoscope/MutationReworks/DarkVisionPatch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using HarmonyLib;
+using Mods.StarlitKaleidoscope.Effects;
+using XRL;
+using XRL.World;
+using XRL.World.Parts.Mutation;
+
+// ReSharper disable UnusedType.Global
+// ReSharper disable UnusedMember.Local
+namespace Mods.StarlitKaleidoscope.MutationReworks {
+    [HarmonyPatch(typeof(DarkVision))]
+    internal static class DarkVisionPatchRadius {
+        static MethodBase TargetMethod() {
+            return AccessTools.Constructor(typeof(DarkVision));
+        }
+        static void Postfix(DarkVision __instance) {
+            __instance.Radius = 8;
+        }
+    }
+}
