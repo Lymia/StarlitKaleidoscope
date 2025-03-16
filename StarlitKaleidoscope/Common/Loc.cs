40ad14d948d84ebd9917551ce43c3120730ec45e
Rimi Kanokawa <rimi@rimin.moe>
2025-03-15 20:53:58 -0700

    Fix serialization for Loc.

--- StarlitKaleidoscope/Common/Loc.cs
+++ StarlitKaleidoscope/Common/Loc.cs
@@ -1,24 +1,34 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
+using XRL.World;
 
 namespace StarlitKaleidoscope.Common {
     [Serializable]
-    public readonly struct Loc : IEquatable<Loc>, IComparable<Loc> {
+    public struct Loc : IEquatable<Loc>, IComparable<Loc>, IComposite {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public Loc(int x, int y) {
             this.x = x;
             this.y = y;
         }
-            
-        public readonly int x, y;
 
+        public int x { get; private set; }
+        public int y { get; private set; }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public bool Equals(Loc other) => x == other.x && y == other.y;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public override bool Equals(object obj) => obj is Loc other && Equals(other);
+
         public override int GetHashCode() => HashCode.Combine(x, y);
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool operator ==(Loc left, Loc right) => left.Equals(right);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool operator !=(Loc left, Loc right) => !left.Equals(right);
-        
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public int CompareTo(Loc other) {
             var xComparison = x.CompareTo(other.x);
             if (xComparison != 0) return xComparison;
@@ -26,6 +36,18 @@ namespace StarlitKaleidoscope.Common {
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public Loc Add(int x, int y) => new Loc(this.x + x, this.y + y);
+        public Loc Add(int x, int y) => new(this.x + x, this.y + y);
+
+        public bool WantFieldReflection => false;
+
+        public void Write(SerializationWriter Writer) {
+            Writer.WriteOptimized(x);
+            Writer.WriteOptimized(y);
+        }
+
+        public void Read(SerializationReader Reader) {
+            x = Reader.ReadOptimizedInt32();
+            y = Reader.ReadOptimizedInt32();
+        }
     }
 }
